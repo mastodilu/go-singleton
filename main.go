ffdecb495ffd6bf547b931542a14ea8da920aa40
Di Lucchio Matteo <matteo.dilucchio@beta80group.it>
2019-09-11 09:29:36 +0200

    add comments and correct Close function

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 
 func main() {
 
+	// test if close actually deletes the singleton and
+	// it can be re-instantiated in the future
 	mydb.New()
+	mydb.Close()
+
+	mydb.New()
+	mydb.New() // this does nothing
 	defer mydb.Close()
 
 	altro.Store("due", "value due")
--- mydb/database.go
+++ mydb/database.go
@@ -5,33 +5,40 @@ import (
 	"sync"
 )
 
-var once sync.Once
+var once sync.Once // allow only one operation to be called
 
-var lock = sync.Mutex{}
-var stored map[string]string
+var lock = sync.Mutex{}      // allow one operation at a time (thread-safe)
+var stored map[string]string // the actual fake db
 
+// New create the new singleton object
 func New() {
 	lock.Lock()
 	defer lock.Unlock()
 
+	// initialize the singleton just once.
+	// future calls of New() won't run this block
 	once.Do(func() {
 		fmt.Println("new class")
 		stored = make(map[string]string)
 	})
 }
 
+// Close delete the singleton object
 func Close() {
 	lock.Lock()
 	defer lock.Unlock()
+	once = sync.Once{}
 	stored = nil
 }
 
+// Store save a key, value pair in the database
 func Store(k, v string) {
 	lock.Lock()
 	defer lock.Unlock()
 	stored[k] = v
 }
 
+// PrintAll print the database
 func PrintAll() {
 	lock.Lock()
 	defer lock.Unlock()
